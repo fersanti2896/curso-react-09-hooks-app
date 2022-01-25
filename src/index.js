f9f8e6f5db3f8a827f880a92beb692b8199a8032
Fernando Nicolas <fersanti2896@gmail.com>
2022-01-25 14:02:49 -0600

    Uso de useState en el componente CounterApp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HookApp } from './HookApp';
+import { CounterApp } from './components/01-useState/CounterApp';
+// import { HookApp } from './HookApp';
 
-ReactDOM.render(<HookApp />, document.getElementById('root'));
+ReactDOM.render(<CounterApp />, document.getElementById('root'));
 
 
